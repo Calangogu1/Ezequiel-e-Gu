4689baa232d87f8d99277252d76630b4f6055bf9
Gustavo <silveiragustavo153@gmail.com>
2022-06-21 22:42:24 -0300

    MiNHAS ALTERAÇÕES DAS CONFIGURAÇÕES DE PERFIL

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-// alert('Olá seja muito Bem-vindo ao Site Drugstore, faça já seu cadastro a baixo e se junte a familía')
+ alert('Olá seja muito Bem-vindo ao Site Drugstore, faça já seu cadastro a baixo e se junte a familía')
+ 
